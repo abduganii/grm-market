0cb639579d72d8926e2d23d17f7956a104c82291
abdiganii <abduganiabdu2@gmail.com>
2025-04-10 21:26:24 +0500

    lkjdks

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -2,14 +2,15 @@ import { fetchData } from "../../service/get";
 import HomePage from "../../views/home";
 
 async function getProduct() {
-  return fetchData(`${process.env.NEXT_PUBLIC_URL}/product/internet-shop`, {
+  return fetchData(`${process.env.NEXT_PUBLIC_URL}/product`, {
     page: 1,
     limit: 12,
-  });
+  })
 }
 
 export default async function Home() {
   const product = await getProduct();
+  console.log(product)
   return (
     <>
       <HomePage product={product?.items} />
